659347675a4d358c3e305336bac840a85a0415e6
unknown <siddharthpadwal3@gmail.com>
2022-02-23 00:33:14 +0530

    Created movie search app using vue with loader effect

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,191 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="app">
+        <header>
+            <h1>The<strong>Movie</strong>Database</h1>
+            <form class="search-box" @submit.prevent="handleSearch">
+                <input
+                    type="search"
+                    class="search-field"
+                    placeholder="Search for a movie..."
+                    required
+                    v-model="search_query"
+                />
+            </form>
+        </header>
+        <main>
+            <div v-if="isActive" :class="{ loader: isActive }"></div>
+            <div v-else>
+                <div class="cards" v-if="movieList.length">
+                    <movieCard
+                        v-for="movie in movieList"
+                        :key="movie.imdbID"
+                        :movie="movie"
+                    />
+                </div>
+                <div class="no-results" v-else>
+                    <h3>Search your movie above...</h3>
+                </div>
+            </div>
+        </main>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from "vue";
+import movieCard from "./components/movieCard";
+import env from "../env";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    components: {
+        movieCard,
+    },
+    setup() {
+        const search_query = ref("");
+        const movieList = ref([]);
+        const isActive = ref(false);
+
+        const handleSearch = async () => {
+            // Need to add loader effect until data gets retrived
+            isActive.value = true;
+
+            movieList.value = await fetch(
+                `https://www.omdbapi.com/?apikey=${env.apikey}&s=${search_query.value}`
+            )
+                .then((res) => res.json())
+                .then((data) =>
+                    !data.Search
+                        ? confirm("Invalid / no matching data found!")
+                        : data.Search
+                );
+            isActive.value = false;
+            console.log(movieList);
+
+            search_query.value = "";
+        };
+        // isActive.value = false;
+
+        return {
+            search_query,
+            movieList,
+            isActive,
+            handleSearch,
+        };
+    },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+
+    font-family: "Trebuchet MS", "Lucida Sans Unicode", "Lucida Grande",
+        "Lucida Sans", Arial, sans-serif;
+}
+
+a {
+    text-decoration: none;
+}
+
+header {
+    padding: 50px 0;
+}
+
+header > h1 {
+    color: #888;
+    font-size: 42px;
+    font-weight: 400;
+    text-align: center;
+    text-transform: uppercase;
+    margin-bottom: 30px;
+}
+
+header > h1 > strong {
+    color: #313131;
+}
+
+header > h1:hover {
+    color: #313131;
+}
+
+.search-box {
+    display: flex;
+    justify-content: center;
+    padding: 0 30px;
+}
+
+.search-field {
+    appearance: none;
+    background: none;
+    border: none;
+    outline: none;
+    border-radius: 8px;
+
+    background-color: #f3f3f3;
+    box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.15);
+
+    display: block;
+    width: 100%;
+    max-width: 600px;
+    padding: 15px;
+
+    color: #313131;
+    font-size: 20px;
+
+    transition: 0.4s;
+}
+
+.search-field::placeholder {
+    color: #aaa;
+}
+
+.search-field:focus,
+.search-field:valid {
+    color: #fff;
+    background-color: #313131;
+    box-shadow: 0px 0px 0px rgba(0, 0, 0, 0.15);
+}
+
+main {
+    max-width: 1200px;
+    margin: 0 auto;
+    padding: 0 30px;
+}
+
+.cards {
+    display: flex;
+    flex-wrap: wrap;
+    margin: 0 -8px;
+}
+
+.no-results {
+    text-align: center;
+}
+
+.loader {
+    border: 16px solid #f3f3f3;
+    border-top: 16px solid #3498db;
+    border-radius: 50%;
+    width: 36px;
+    height: 36px;
+    margin: 0 auto;
+    animation: spin 2s linear infinite;
+}
+
+@keyframes spin {
+    0% {
+        transform: rotate(0deg);
+    }
+    100% {
+        transform: rotate(360deg);
+    }
+}
+
+@media screen and (max-width: 688px) {
+    .cards {
+        display: unset;
+        flex-wrap: unset;
+    }
 }
 </style>
---
+++ src/components/movieCard.vue
@@ -0,0 +1,75 @@
+<template>
+    <div class="card">
+        <img
+            :src="movie.Poster"
+            @error="setAltImg"
+            :alt="movie.Title + ' Poster'"
+        />
+        <h3>
+            {{ movie.Title }} <span>{{ movie.Year }}</span>
+        </h3>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ["movie"],
+    setup() {
+        const setAltImg = (event) => {
+            event.target.src =
+                "https://www.freeiconspng.com/uploads/no-image-icon-15.png";
+        };
+        return {
+            setAltImg,
+        };
+    },
+};
+</script>
+
+<style>
+.card {
+    flex: 1 1 33%;
+    width: 33%;
+    max-width: 33%;
+    padding: 0 8px;
+    margin-bottom: 16px;
+}
+
+img {
+    width: 100%;
+    height: 300px;
+    object-fit: cover;
+    border-radius: 16px;
+    box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.15);
+    transition: 0.4s;
+}
+
+h3 {
+    padding: 16px;
+    color: #313131;
+    transition: 0.4s;
+}
+
+img:hover {
+    transform: scale(1.05);
+}
+
+h3:hover {
+    color: #24c444;
+}
+
+h3 > span {
+    display: block;
+    color: #a0a0a0;
+    font-size: 14px;
+    font-weight: 400;
+}
+
+@media screen and (max-width: 688px) {
+    .card {
+        flex: unset;
+        width: unset;
+        max-width: unset;
+    }
+}
+</style>
